fb9493e44551db4aa8112a2d9c36ec1ef56859a7
Nikola <ncurilovic@gmail.com>
2021-06-03 17:09:58 +0200

    Extract project build.gradle versions to gradle version catalog

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,9 +4,12 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.0-beta03")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.10")
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.36")
+        val libs = project.extensions.getByType<VersionCatalogsExtension>()
+            .named("libs") as org.gradle.accessors.dm.LibrariesForLibs
+
+        classpath(libs.android.gradle.plugin)
+        classpath(libs.kotlin.gradle.plugin)
+        classpath(libs.hilt.gradle.plugin)
     }
 }
 
