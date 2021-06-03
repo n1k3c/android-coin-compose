f0598fb6f28161218b3bd872048843d18ecb0a98
Nikola <ncurilovic@gmail.com>
2021-06-03 16:58:04 +0200

    Upgrade hilt to 2.36

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,16 +2,11 @@ buildscript {
     repositories {
         google()
         mavenCentral()
-        maven(url = "https://oss.sonatype.org/content/repositories/snapshots") {
-            content {
-                includeModule("com.google.dagger", "hilt-android-gradle-plugin")
-            }
-        }
     }
     dependencies {
         classpath("com.android.tools.build:gradle:7.0.0-beta03")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.10")
-        classpath("com.google.dagger:hilt-android-gradle-plugin:HEAD-SNAPSHOT")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.36")
     }
 }
 
