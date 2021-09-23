81feda3a297a9b93d45e860b750c4a9bc063e2c8
Nikola <ncurilovic@gmail.com>
2021-09-23 16:08:10 +0200

    Code cleanup

--- features/news/src/main/java/com/nikec/coincompose/news/ui/list/NewsContent.kt
+++ features/news/src/main/java/com/nikec/coincompose/news/ui/list/NewsContent.kt
@@ -2,7 +2,6 @@ package com.nikec.coincompose.news.ui.list
 
 import android.text.format.DateUtils
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
