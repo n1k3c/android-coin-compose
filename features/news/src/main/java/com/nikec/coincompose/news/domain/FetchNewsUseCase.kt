72f6f9347c51bbe9b9bb24f10d652ffaba388427
Nikola <ncurilovic@gmail.com>
2021-10-04 10:35:23 +0200

    Fix loading news images

--- features/news/src/main/java/com/nikec/coincompose/news/domain/FetchNewsUseCase.kt
+++ features/news/src/main/java/com/nikec/coincompose/news/domain/FetchNewsUseCase.kt
@@ -8,6 +8,7 @@ import com.nikec.coincompose.news.data.repository.NewsRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class FetchNewsUseCase @Inject constructor(
@@ -18,7 +19,9 @@ class FetchNewsUseCase @Inject constructor(
     fun execute(): Flow<PagingData<News>> {
         return newsRepository.fetchNews().map { pagingData ->
             pagingData.map { news ->
-                val newsImage = newsRepository.fetchNewsImage(news)
+                val newsImage = withContext(coroutineContextProvider.io) {
+                    newsRepository.fetchNewsImage(news)
+                }
                 news.copy(image = newsImage)
             }
         }.flowOn(coroutineContextProvider.io)
